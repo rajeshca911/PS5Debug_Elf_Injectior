e66dace8b294653e1312d10f481edd17bce02887
rajeshca911 <97240423+rajeshca911@users.noreply.github.com>
2024-01-15 11:36:43 +0530

    Homeform

--- PS5Debug_Elf_Injectior/My Project/Application.Designer.vb
+++ PS5Debug_Elf_Injectior/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.PS5Debug_Elf_Injectior.PlistExpl
+            Me.MainForm = Global.PS5Debug_Elf_Injectior.HomeForm
         End Sub
     End Class
 End Namespace
